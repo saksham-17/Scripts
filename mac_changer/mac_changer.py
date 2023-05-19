76146d0c8582e570973b494ad5cce76fa8d4fe2a
Saksham Bhattarai <unfriend33@gmail.com>
2023-05-19 20:37:42 +0545

    Handle command line arguments
    Able to handle command line arguments 
    Minor change

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env Python
 
 import subprocess
+import optparse
 
-interface = input("network interface> ")
-address = input("mac address> ")
-print("[+] Changing mac address for " + interface + " to " + address)
+parser = optparse.OptionParser()
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its mack address")
+parser.add_option("-m", "--mac_address", dest="address", help="New mac address")
+
+(options, arguments) = parser.parse_args()
+interface = options.interface
+address = options.address
+print("\n[+] Changing mac address for " + interface + " to " + address)
 
 subprocess.call(["ifconfig", interface, "down"])
 subprocess.call(["ifconfig", interface, "hw", "ether", address])
 subprocess.call(["ifconfig", interface, "up"])
-subprocess.call(["ifconfig", interface])
+

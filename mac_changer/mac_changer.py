339b2d5bfa3c5e180e16eebc3cfb8d40934c14ff
Saksham Bhattarai <unfriend33@gmail.com>
2023-05-20 17:33:17 +0545

    Final
    Added code to handle error
    User friendly

--- mac_changer/mac_changer.py
+++ mac_changer/mac_changer.py
@@ -2,6 +2,8 @@
 
 import subprocess
 import optparse
+import re
+
 
 def get_arguments():
     parser = optparse.OptionParser()
@@ -14,14 +16,37 @@ def get_arguments():
         parser.error("[-] Please enter the mac address, use --help for more info ")
     return options
 
+
 def change_mac(interface,address):
     print("\n[+] Changing mac address for " + interface + " to " + address)
     subprocess.call(["ifconfig", interface, "down"])
     subprocess.call(["ifconfig", interface, "hw", "ether", address])
     subprocess.call(["ifconfig", interface, "up"])
 
+
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface])
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] Could not read MAC address.")
+
+
 options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print("Current MAC = " + str(current_mac))
 change_mac(options.interface, options.address)
+current_mac = get_current_mac(options.interface)
+
+if current_mac == options.address:
+    print("[+] MAC address was successfully changed to " + current_mac)
+else:
+    print("[-] MAC address didn't changed.")
+
+
+
+
 
 
 
